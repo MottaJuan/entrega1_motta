5e1327260a3a01cf4852b87f74016cbc90d79694
Juan Motta <mottajuann@gmail.com>
2022-09-07 02:40:12 -0300

    mostrar equipos

---
+++ app_1/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from app_1.views import probar
+
+urlpatterns = [
+    path('',probar),
+]
--- app_1/views.py
+++ app_1/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import Template, Context, loader
+from app_1.models import Jugador, Entrenador, Equipo
 
-# Create your views here.
+def probar(request):
+    queryset=Equipo.objects.all()
+    diccionario={'app_1':queryset}
+    plantilla=loader.get_template('plantilla1.html')
+    documento_html=plantilla.render(diccionario)
+
+    return HttpResponse(documento_html)
