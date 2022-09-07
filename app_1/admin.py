70df68a43ba2ad140b25a445ec882d56addffad5
Juan Motta <mottajuann@gmail.com>
2022-09-07 13:44:02 -0300

    agregado admin a app_1

--- app_1/admin.py
+++ app_1/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from app_1.models import Jugador, Entrenador, Equipo
 
-# Register your models here.
+
+admin.site.register(Jugador)
+admin.site.register(Entrenador)
+admin.site.register(Equipo)
